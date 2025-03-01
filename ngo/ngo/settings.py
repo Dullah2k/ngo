7b7a8193920414722253615ab905e4c8404485df
Dullah <dullahmarlabu@gmail.com>
2025-03-01 12:13:25 +0300

    migrated to postgresql database

--- ngo/ngo/settings.py
+++ ngo/ngo/settings.py
@@ -77,10 +77,12 @@ WSGI_APPLICATION = 'ngo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'ngo_db',
+    'USER': 'ngo',
+    'PASSWORD': 'qwerty123',
+  }
 }
 
 
