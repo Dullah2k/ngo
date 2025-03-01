1369ede3f8625dc5e4af2220ec1a05dba83495b3
Dullah <dullahmarlabu@gmail.com>
2025-03-01 11:58:35 +0300

    modified the dashboard

--- ngo/ngo/settings.py
+++ ngo/ngo/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'user_auth.apps.UserAuthConfig',
-    'landing_page.apps.LandingPageConfig',
-    'organization.apps.OrganizationConfig'
+  'user_auth.apps.UserAuthConfig',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+  'landing_page.apps.LandingPageConfig',
+  'organization.apps.OrganizationConfig'
 ]
 
 MIDDLEWARE = [
--- ngo/user_auth/urls.py
+++ ngo/user_auth/urls.py
@@ -4,7 +4,6 @@ from .import views
 
 app_name = "user_auth"
 urlpatterns = [
-#   path('create-account/', views.register_user, name="register_user"),
   path('login/', auth_views.LoginView.as_view(), name="login"),
   path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]
